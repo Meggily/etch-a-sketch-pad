9b3c4e44b219346140a937dda3d8ea4d53e1b5d5
Meggily <Meggily2722@gmail.com>
2023-05-04 19:49:33 -0400

    finish game, in working order-needs improvements

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const form= document.querySelector('form');
 const gridInputSize= document.querySelector('.grid-input-size');
 const gridDiv= document.querySelector('.grid-div'); 
 const gridSquare = document.querySelectorAll('.grid-square');
+const colorPicked = document.querySelector('.color-picker').value;
 const DEFAULT_GRID_SIZE = 16;
 const DEFAULT_COLOR = '#8fbc8f';
 let gridSize = DEFAULT_GRID_SIZE;
 let newColor = DEFAULT_COLOR;
 
-//default grid
+// default grid
 
 for (let i = 0; i< gridSize*gridSize; i++){
     let grid= document.createElement('div');
     grid.className='grid-square';
-    grid.style.height = 480/gridSize + 'px';
-    grid.style.width = 480/gridSize + 'px';
+    grid.style.height = 650/gridSize + 'px';
+    grid.style.width = 650/gridSize + 'px';
     grid.style.backgroundColor = DEFAULT_COLOR;
     gridDiv.appendChild(grid);
 
@@ -23,9 +24,16 @@ for (let i = 0; i< gridSize*gridSize; i++){
 let newGridSize = function(event) {
     event.preventDefault();
     gridSize= document.querySelector('#grid-size').value
+    if (gridSize == "")
+    return;
+    else if (gridSize > 64){
+        gridSize = 64;
+    }else if (gridSize < 0){
+        gridSize = 2;
+    }
+
     gridInputSize.innerText = `Grid size chosen: ${gridSize} x ${gridSize}`;
     newGridSize=gridSize;
-    console.log(newGridSize)
 
 
     createGrid();
@@ -43,12 +51,11 @@ let newGridSize = function(event) {
         for (let i = 0; i< gridSize*gridSize; i++){
         let grid= document.createElement('div');
         grid.className='grid-square';
-        grid.style.height = 480/gridSize + 'px';
-        grid.style.width = 480/gridSize + 'px';
+        grid.style.height = 650/gridSize + 'px';
+        grid.style.width = 650/gridSize + 'px';
         grid.style.backgroundColor = DEFAULT_COLOR;
         gridDiv.appendChild(grid);
 
-        
         }
     }
 }
@@ -56,5 +63,45 @@ let newGridSize = function(event) {
 form.addEventListener('submit', newGridSize);
 
 
+document.addEventListener('mousedown', function(event){
+
+  
+    if (event.target.matches('.grid-square')){
+        event.target.style.backgroundColor = 'black';
+    }
+        
+})
+
+function changeColor(){
+
+    document.querySelector(".color-picker").addEventListener('input', (e) => {
+        newColor = e.target.value;
+
+        document.addEventListener('mousedown', function(event){
+
+  
+        if (event.target.matches('.grid-square')){
+            event.target.style.backgroundColor = newColor;
+        }
+
+})
+
+    });
+};
+changeColor();
 
 
+document.querySelector('.erase').addEventListener('click', function(event){
+    if (event.target.matches('.erase')){
+        newColor = DEFAULT_COLOR;
+    }
+    document.addEventListener('mousedown', function(event){
+
+  
+        if (event.target.matches('.grid-square')){
+            event.target.style.backgroundColor = newColor;
+        }
+
+})
+    
+})
